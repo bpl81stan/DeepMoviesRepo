080c50de5d884eed46fbb2a5d9043cb9c2845d53
Brent LeBlanc <brent.p.leblanc@boeing.com>
2020-02-13 16:19:22 -0800

    Added linear and logistic regression. Modified 25M dataset to pass embeddings to regression models.

--- models/ML_Logistic_Regression.py
+++ models/ML_Logistic_Regression.py
@@ -1,12 +1,15 @@
 import sklearn
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 import util.data_aquire as ml_data
 from sklearn import linear_model
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from collections import Counter
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score, mean_absolute_error, confusion_matrix, accuracy_score
 
-movie_features = ml_data.create_100k_embeddings()
+#movie_features = ml_data.create_100k_embeddings()
+movie_features = ml_data.create_25M_embeddings()
 
 # print features
 for x in movie_features.columns:
@@ -14,7 +17,10 @@ for x in movie_features.columns:
 
 
 
-x = movie_features.drop(['user_id','item_id','rating'], axis=1)
+#x = movie_features.drop(['user_id','item_id','rating'], axis=1)
+
+x = movie_features.drop(['userId','movieId','rating'], axis=1)
+
 y = movie_features['rating']
 
 print("Length of X:")
@@ -22,8 +28,10 @@ print("Length of X:")
 print(len(x))
 
 print("Length of Y:")
-
 print(len(y))
+print(min(y))
+print(max(y))
+print(Counter(y))
 
 movie_X_train, movie_X_test, movie_y_train, movie_y_test = train_test_split(x, y, test_size=0.05, random_state=42)
 
@@ -39,11 +47,8 @@ print("Length of Y-test:")
 print(movie_y_test.shape)
 
 
-
-
-
 # Create linear regression object
-regr = linear_model.LinearRegression()
+regr = linear_model.LogisticRegression(max_iter=5000)
 
 # Train the model using the training sets
 regr.fit(movie_X_train, movie_y_train)
@@ -53,11 +58,19 @@ movie_y_pred = regr.predict(movie_X_test)
 
 print("Length of y-pred:")
 print(movie_y_pred.shape)
+print(min(movie_y_pred))
+print(max(movie_y_pred))
+print(Counter(movie_y_pred))
 
 print('Coefficients: \n', regr.coef_)
+
+print('Mean Absolute Error: %.2f'
+      % mean_absolute_error(movie_y_test, movie_y_pred))
+
 # The mean squared error
 print('Mean squared error: %.2f'
       % mean_squared_error(movie_y_test, movie_y_pred))
+
 # The coefficient of determination: 1 is perfect prediction
 print('Coefficient of determination: %.2f'
       % r2_score(movie_y_test, movie_y_pred))
@@ -65,4 +78,8 @@ print('Coefficient of determination: %.2f'
 print('Explained variance score : %.2f'
       % explained_variance_score(movie_y_test, movie_y_pred))
 
+print('Accuracy Score : %.2f'
+      % accuracy_score(movie_y_test, movie_y_pred))
 
+print('Confusion Matrix:')
+print(confusion_matrix(movie_y_test, movie_y_pred))
--- util/data_aquire.py
+++ util/data_aquire.py
@@ -47,6 +47,7 @@ def get_data_file(filename, dataset, path, print_head=True):
 
     if print_head:
         print(df.head())
+        print(df.shape)
 
     return df
 
@@ -64,18 +65,13 @@ def create_100k_embeddings():
 
     users, ratings, movies = get_100k_data()
 
-    #cols_to_use = ratings.columns.difference(movies.columns)
     user_movie_embedding = pd.merge(ratings, movies, left_on='item_id', right_on='movie_id',
                                     how='left', suffixes=['_ratings', '_movies'])
 
-    #cols_to_use = user_movie_embedding.columns.difference(users.columns)
     user_movie_embedding = pd.merge(user_movie_embedding, users, left_on='user_id', right_on='user_id',
                                     how='left', suffixes=['_ratings', '_users'])
 
-    #user_movie_embedding['datetime'] = datetime.utcfromtimestamp(int(user_movie_embedding['timestamp'])).strftime('%Y-%m-%d %H:%M:%S')
     user_movie_embedding['datetime'] = pd.to_datetime(user_movie_embedding['timestamp'], unit='s')
-
-
 
     user_movie_embedding['days_after_release_rating']=\
                                                     (
@@ -114,33 +110,81 @@ def create_100k_embeddings():
 
     user_movie_embedding = user_movie_embedding.loc[:, [x in keep_columns for x in user_movie_embedding.columns]]
 
-    # print(user_movie_embedding)
-
     user_movie_embedding = pd.get_dummies(user_movie_embedding)
 
     user_movie_embedding = user_movie_embedding.fillna(0)
 
-    # print(user_movie_embedding)
     user_movie_embedding.to_csv(os.path.join(ROOT_DIR,'data','ml-100k','embeddings.csv'))
 
     return user_movie_embedding
 
 
-def get_25M_data(path=r'data\ml-25m', print_head=True):
+def create_25M_embeddings():
 
-    directory= path
+    genome_scores, genome_tags, links, movies, ratings, tags = get_25M_data()  # , overviews
 
-        #r'C:\Users\brent\Documents\Projects\fastai\my-movie-recommender\data\ml-25m'
+    user_movie_embedding = pd.merge(ratings, movies,
+                                    left_on='movieId', right_on='movieId', how='left',
+                                    suffixes=['_ratings', '_movies'])
+
+    # user_movie_embedding['datetime'] = pd.to_datetime(user_movie_embedding['timestamp'], unit='s')
+    #
+    # user_movie_embedding['days_after_release_rating'] = \
+    #     (
+    #             pd.to_datetime(user_movie_embedding['datetime']) -
+    #             pd.to_datetime(user_movie_embedding['release_date'])
+    #     ).dt.days
+
+    keep_columns = ['userId',
+                    'movieId',
+                    'rating',
+                    '(no genres listed)',
+                    'Action',
+                    'Adventure',
+                    'Animation',
+                    'Children',
+                    'Comedy',
+                    'Crime',
+                    'Documentary',
+                    'Drama',
+                    'Fantasy',
+                    'Film-Noir',
+                    'Horror',
+                    'IMAX',
+                    'Musical',
+                    'Mystery',
+                    'Romance',
+                    'Sci-Fi',
+                    'Thriller',
+                    'War',
+                    'Western'
+                    ]
+    #                    'zip_code']
+
+    user_movie_embedding = user_movie_embedding.loc[:, [x in keep_columns for x in user_movie_embedding.columns]]
+
+    #user_movie_embedding = pd.get_dummies(user_movie_embedding)
+
+    user_movie_embedding = user_movie_embedding.fillna(0)
+
+    user_movie_embedding.to_csv(os.path.join(ROOT_DIR, 'data', 'ml-25m', 'embeddings.csv'))
+
+    return user_movie_embedding
+
+
+def get_25M_data(print_head=True):
+
+    path = os.path.join(ROOT_DIR, 'data', 'ml-25m')
 
     # read in 1. genome_scores
-    path_data = os.path.join(directory, 'genome-scores.csv')
+    path_data = os.path.join(path, 'genome-scores.csv')
     genome_scores = pd.read_csv(path_data, sep=",", header=0)
     if print_head:
         print("genome_scores:")
         print(genome_scores.head())
 
     # read in 2. genome_tags
-    path_data = os.path.join(directory, 'genome-tags.csv')
+    path_data = os.path.join(path, 'genome-tags.csv')
     genome_tags = pd.read_csv(path_data, sep=",", header=0)
 
     if print_head:
@@ -148,7 +192,7 @@ def get_25M_data(path=r'data\ml-25m', print_head=True):
         print(genome_tags.head())
 
     # read in 3. links
-    path_data = os.path.join(directory, 'links.csv')
+    path_data = os.path.join(path, 'links.csv')
     links = pd.read_csv(path_data, sep=",", header=0)
 
     if print_head:
@@ -156,8 +200,10 @@ def get_25M_data(path=r'data\ml-25m', print_head=True):
         print(links.head())
 
     # read in 4. movies
-    path_data = os.path.join(directory, 'movies.csv')
+    path_data = os.path.join(path, 'movies.csv')
     movies = pd.read_csv(path_data, sep=",", header=0)
+
+    movies = get_genres(movies)
 
     if print_head:
         print("movies:")
@@ -165,7 +211,7 @@ def get_25M_data(path=r'data\ml-25m', print_head=True):
         print(movies.columns)
 
     # read in 5. ratings
-    path_data = os.path.join(directory, 'ratings.csv')
+    path_data = os.path.join(path, 'ratings.csv')
     ratings = pd.read_csv(path_data, sep=",", header=0)
 
     if print_head:
@@ -173,7 +219,7 @@ def get_25M_data(path=r'data\ml-25m', print_head=True):
         print(ratings.head())
 
     # read in 6. tags
-    path_data = os.path.join(directory, 'tags.csv')
+    path_data = os.path.join(path, 'tags.csv')
     tags = pd.read_csv(path_data, sep=",", header=0)
 
     if print_head:
@@ -181,19 +227,41 @@ def get_25M_data(path=r'data\ml-25m', print_head=True):
         print(tags.head())
 
     # read in 7. overview
-    imdb_directory=r'data\imdb'
-    path_data = os.path.join(directory, 'movie_overview.csv')
-    overviews = pd.read_csv(path_data, sep=",", header=0)
+    # imdb_path=  os.path.join(ROOT_DIR, 'data', 'imdb', 'movie_overview.csv')
+    #
+    # if not os.path.exists(imdb_path):
+    #     aquire_overviews(links)
+    #
+    # overviews = pd.read_csv(imdb_path, sep=",", header=0)
+    #
+    # if print_head:
+    #     print("overviews:")
+    #     print(overviews.head())
 
-    if print_head:
-        print("overviews:")
-        print(overviews.head())
+    return genome_scores, genome_tags, links, movies, ratings, tags#, overviews
 
-    return genome_scores, genome_tags, links, movies, ratings, tags, overviews
+def get_genres(movies):
+
+    df = pd.DataFrame(movies.iloc[:,2]) # genre's column in dataset
+
+    genres = (df.genres.str.split('\s*,\s*', expand=True)
+               .stack()
+               .str.get_dummies()
+               .sum(level=0))
+
+    #print(genres)
+
+    movies = movies.merge(genres, left_index=True, right_index=True)
+
+    return movies
+
+
 
 def get_tmdb_overview(tmdb_id):
 
-    keys = pd.read_csv(r'auth_keys\api_keys.csv', sep=',', index_col=False)
+    path = os.path.join(ROOT_DIR,'auth_keys','api_keys.csv')
+
+    keys = pd.read_csv(path, sep=',', index_col=False)
 
     api_key = keys.loc[0]['key']
 
@@ -214,11 +282,8 @@ def get_tmdb_overview(tmdb_id):
         return ret_overview
 
 
-def set_env_key_value(key, value):
-
-    os.environ[key]=value
-
 def aquire_overviews(links):
+
     tmbd_overview = pd.DataFrame(columns=['movieId', 'imdbId', 'tmbdId', 'overview'])
 
     j = 0
@@ -249,7 +314,9 @@ def aquire_overviews(links):
             j = 0
             print(str(i + 1) + str(":") + str(round(float(i) / float(count) * 100., 2)) + str("%"))
 
-    tmbd_overview.to_csv('data/imdb/movie_overview.csv', index=False, index_label=False)
+    out_path = os.path.join(ROOT_DIR, 'data', 'imdb','movie_overview.csv')
+
+    tmbd_overview.to_csv(out_path, index=False, index_label=False)
 
 
 # def main():
@@ -257,7 +324,7 @@ def aquire_overviews(links):
 #     #                     path=r'C:\Users\brent\Documents\Projects\fastai\my-movie-recommender\data\ml-25m',
 #     #                     print_head=False
 #     # )
-#     user_movie_embedding = create_100k_embeddings()
+#     user_movie_embedding = create_25M_embeddings()
 #
 #     print(user_movie_embedding)
 #
